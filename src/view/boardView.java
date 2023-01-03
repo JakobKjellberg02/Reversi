f4424de5c123a7e6cbadaee0ee9e85fe04bfc9a6
Jakob Zornig Kjellberg <jakob.zornigkjellberg@gmail.com>
2023-01-03 16:50:06 +0100

    Comments and tests

--- src/view/boardView.java
+++ src/view/boardView.java
@@ -8,16 +8,20 @@ public class boardView {
         boardModel BOARD = new boardModel(8);
         char[][] board = BOARD.initializeBoard();
         printBoard(board);
-        System.out.println("");
         BOARD.turn(4, 5, 'B');
         printBoard(board);
         BOARD.turn(5, 3, 'W');
         printBoard(board);
         BOARD.turn(3, 2, 'B');
         printBoard(board);
+        BOARD.turn(4, 6, 'W');
+        printBoard(board);
+        BOARD.turn(5, 4, 'B');
+        printBoard(board);
     }
 
     private  static void printBoard(char[][] b) {
+        System.out.println("");
         int size = b.length;
         for (int row = 0; row < size; row++) {
             for (int col = 0; col < size; col++) {
