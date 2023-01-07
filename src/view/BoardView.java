4741fccba75caf6a767408e1705615cb358089a6
s216164 <s216164@student.dtu.dk>
2023-01-07 15:26:41 +0100

    Har gjort brikkerne runde

--- src/view/BoardView.java
+++ src/view/BoardView.java
@@ -2,11 +2,25 @@ package view;
 import controller.BoardController;
 import controller.PlayerController;
 import javafx.application.Application;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
+import javafx.scene.layout.Background;
+import javafx.scene.layout.BackgroundFill;
+import javafx.scene.layout.Border;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.BorderStroke;
+import javafx.scene.layout.BorderStrokeStyle;
+import javafx.scene.layout.BorderWidths;
+import javafx.scene.layout.CornerRadii;
 import javafx.scene.layout.GridPane;
+import javafx.scene.paint.Color;
+import javafx.scene.paint.Paint;
+import javafx.scene.shape.Circle;
+import javafx.scene.text.Font;
+import javafx.scene.text.FontWeight;
 import javafx.stage.Stage;
 import model.BoardModel;
 import model.PlayerModel;
@@ -43,7 +57,11 @@ public class BoardView extends Application{
     //Center piece of the screen - The Board
     board_gui = new Button[BOARD_SIZE][BOARD_SIZE];
     GridPane gridPane = new GridPane();
-    gridPane.setPrefSize(600, 600);
+    //adjusts the size of the board
+    gridPane.setPrefSize(800, 800);
+    //Show grid lines
+  gridPane.setGridLinesVisible(true);
+  //for loop that adds buttons to the board
     for (int i = 0; i < BOARD_SIZE; i++) {
       for (int j = 0; j < BOARD_SIZE; j++) {
         // tilføjer knapper i hvert kvadrat
@@ -61,6 +79,9 @@ public class BoardView extends Application{
     passButton = new Button();
     passButton.setText("Pass");
     passButton.setId("0");
+    passButton.setFont(Font.font("Times New Roman", FontWeight.BOLD, 18));
+    passButton.setBackground(new Background(new BackgroundFill(Color.LIGHTGRAY, CornerRadii.EMPTY, Insets.EMPTY)));
+    passButton.setBorder(new Border(new BorderStroke(Color.BLACK, BorderStrokeStyle.SOLID, CornerRadii.EMPTY, BorderWidths.DEFAULT)));
     passButton.setOnAction(playerController.getEventHandler());
     bPane.setBottom(passButton);
 
@@ -68,6 +89,14 @@ public class BoardView extends Application{
     score = new TextField();
     score.setText(players[turn].getName() + " " + players[turn].getPoints() + " " + players[turn].getColor());
     bPane.setTop(score); 
+    //Changes the font
+    score.setFont(Font.font("Times New Roman", FontWeight.BOLD, 20));
+    //Adds a background color
+    score.setBackground(new Background(new BackgroundFill(Color.ORANGE, CornerRadii.EMPTY, Insets.EMPTY)));
+    //Adds a border
+    score.setBorder(new Border(new BorderStroke(Color.BLACK, BorderStrokeStyle.SOLID, CornerRadii.EMPTY, BorderWidths.DEFAULT)));
+    //Change the alignment of the text
+    score.setAlignment(Pos.CENTER);
 
     //Combines it all to the scene
     Scene scene = new Scene(bPane, 500, 500);
@@ -105,16 +134,24 @@ public class BoardView extends Application{
 
   //Method for changing from char to a GUI color 
   public static void changeColor(Button[][] button, BoardModel BOARDMODEL, char[][] board_data, int i, int j) {
+    //if statement that creates circles
     if (BOARDMODEL.getID(i,j,board_data) == 'B') {
-     button[i][j].setStyle("-fx-background-color: #000000");
+       Circle circleB = new Circle(22);
+       circleB.setFill(Paint.valueOf("#000000"));
+       button[i][j].setGraphic(circleB);
     } else if (BOARDMODEL.getID(i,j,board_data) == 'W') {
-     button[i][j].setStyle("-fx-background-color: #fafcfa");
-    } else {
+      Circle circleW = new Circle(22);
+      circleW.setFill(Paint.valueOf("#fafcfa"));
+      button[i][j].setGraphic(circleW);
+    } 
+    //if statement that paint the boards background color
       if ((i + j) % 2 == 0) {
         button[i][j].setStyle("-fx-background-color: #023602");  
       } else {
        button[i][j].setStyle("-fx-background-color: #046e04");  
      }
-    }
+    
   }
+
+  
 }
